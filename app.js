c4b72583d9039e82c686b2f996300d6a64bb1e05
Daisuke.Yagi <yagi_daisuke@mindfree.jp>
2012-08-20 09:55:17 +0900

    initail commit

---
+++ app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+"use strict";
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/game', routes.game);
+app.get('/gameover', routes.gameover);
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server,{'log level':1})
+var _userId = 0;
+io.sockets.on('connection',function(socket){
+	socket.handshake.userId = _userId;
+	_userId ++;
+	socket.on('player-update',function(data){
+		socket.broadcast.json.emit('player-update',{userId:socket.handshake.userId,data:data});
+	});
+	socket.on('bullet-create',function(data){
+		socket.broadcast.json.emit('bullet-create',{userId:socket.handshake.userId,data:data});
+	});
+	socket.on('disconnect',function(){
+		socket.broadcast.json.emit('disconnect-user',{userId:socket.handshake.userId});
+	});
+});
---
+++ public/javascripts/client.js
@@ -0,0 +1,138 @@
+jQuery(function($) {
+	"use strict";
+	var _socket = io.connect('http://'+location.host+'/');
+	var _userMap = {};
+	var _bulletMap = {};
+	_socket.on('player-update',function(data){
+		var user;
+		if(_userMap[data.userId] === undefined){
+			console.log('create user '+data.userId , data);
+			user = {x:0,y:0,v:0,rotate:0,userId:data.userId};
+			user.element = $('<img src="/images/unit.png" class="player" />')
+				.attr('data-user-id',user.userId);
+			$('body').append(user.element);
+			_userMap[data.userId] = user;
+			
+			var bullet = {x:-100,y:-100,v:0,rotate:0,userId:data.userId};
+			bullet.element = $('<img src="/images/bullet.png" class="bullet" />')
+				.attr('data-user-id',user.userId);
+			$('body').append(bullet.element);
+			
+			_bulletMap[data.userId] =bullet;
+			
+		}else{
+			user = _userMap[data.userId];
+		}
+		user.x = data.data.x;
+		user.y = data.data.y;
+		user.rotate = data.data.rotate;
+		user.v = data.data.v;
+		updateCss(user);
+	});
+	_socket.on('bullet-create',function(data){
+		var bullet = _bulletMap[data.userId];
+		if(bullet !== undefined){
+			bullet.x = data.data.x;
+			bullet.y = data.data.y;
+			bullet.rotate = data.data.rotate;
+			bullet.v = data.data.v;
+		}
+	});
+	
+	_socket.on('disconnect-user',function(data){
+		var user = _userMap[data.userId];
+		if(user !== undefined){
+			user.element.remove();
+			delete _userMap[data.userId];
+			var bullet = _bulletMap[data.userId];
+			bullet.element.remove();
+			delete _bulletMap[data.userId];
+		}
+		
+	});
+	
+	
+	var _keyMap = [];
+	var _player = {x:Math.random()*1000|0,y:Math.random()*500|0,v:0,rotate:0,element:$('#my-player')};
+	var _bullet = {x:-100,y:-100,v:0,rotate:0,element:$('#my-bullet')};
+	
+	var updatePosition = function(unit){
+		unit.x += unit.v* Math.cos(unit.rotate * Math.PI /180);
+		unit.y += unit.v* Math.sin(unit.rotate * Math.PI /180);
+		
+	};
+	var updateCss = function(unit){
+		unit.element.css({
+			left:unit.x|0+'px',
+			top:unit.y|0 + 'px',
+			transform:'rotate('+unit.rotate+'deg)'
+		});
+	};
+	var f = function(){
+		if(_keyMap[37] === true){
+			//left
+			_player.rotate -= 3;
+		}
+		if(_keyMap[38] === true){
+			//up
+			_player.v += 0.5;
+		}
+		if(_keyMap[39] === true){
+			//right
+			_player.rotate += 3;
+		}
+		if(_keyMap[40] === true){
+			//down
+			_player.v -= 0.5;
+		}
+		if(_keyMap[32] === true && _isSpaceKeyUp){
+			//space
+			_isSpaceKeyUp = false;
+			_bullet.x = _player.x +20;
+			_bullet.y = _player.y +20;
+			_bullet.rotate = _player.rotate;
+			_bullet.v = Math.max(_player.v + 3,3);
+			_socket.emit('bullet-create',{
+				x:_bullet.x|0,
+				y:_bullet.y|0,
+				rotate:_bullet.rotate|0,
+				v:_bullet.v
+			});
+		}
+		_player.v *= 0.95;
+		updatePosition(_player);
+		var w_width = $(window).width();
+		var w_height =$(window).height();
+		if(_player.x < -50){ _player.x = w_width;}
+		if(_player.y < -50){ _player.y = w_height}
+		if(_player.x > w_width){_player.x = -50;}
+		if(_player.y > w_height){_player.y = -50;}
+		
+		updatePosition(_bullet);
+		Object.keys(_bulletMap).forEach(function(key){
+			var bullet = _bulletMap[key];
+			updatePosition(bullet);
+			updateCss(bullet);
+			if(_player.x < bullet.x && bullet.x <_player.x + 50 &&
+			_player.y < bullet.y && bullet.y <_player.y + 50){
+				location.href = '/gameover'
+			}
+			
+		});
+		updateCss(_bullet);
+		updateCss(_player);
+		_socket.emit('player-update',{x:_player.x|0,y:_player.y|0,rotate:_player.rotate|0,v:_player.v});
+		setTimeout(f,30);
+	};
+	var _isSpaceKeyUp = true;
+	setTimeout(f,30);
+	$(window).keydown(function(e){
+		_keyMap[e.keyCode] = true;
+	});
+	$(window).keyup(function(e){
+		if(e.keyCode === 32){
+			_isSpaceKeyUp = true;
+		}
+		_keyMap[e.keyCode] = false;
+	});
+});
